e3e77d4e85d9efa2e747369ba695b5882d2400aa
Anoop Shukla <anoopshukla@Rishabhs-MacBook-Pro.local>
2025-12-16 19:12:03 +0530

    Added Recursion problems

--- Codes/LearnTheBasic/BasicRecursion/recursionProblems.py
+++ Codes/LearnTheBasic/BasicRecursion/recursionProblems.py
@@ -15,7 +15,41 @@ class RecursionProblems:
         else:
             return self.fib(n-1) + self.fib(n-2)
 
+    def printNum(self,n):
+        if n == 1:
+            return 1
+        print(n, end = " ")
+        return self.printNum(n-1)
+    
+    def ascPrintNum(self, i ,n):
+        if i ==n :
+            return n
+        print(i,end = " ")
+        return self.ascPrintNum(i+1,n)
+        # print("i",i)
+
+# Sum of first N numbers
+    def sumFirstNum(self,n):
+        if n <= 0:
+            return 0
+        return n + self.sumFirstNum(n-1)
+
+#Reverse an Array
+    def reverse(self,arr):
+        return(arr[::-1])
+    
+    def recReverse(self,arr,left ,right):
+        if left >= right :
+            return
+        arr[left],arr[right] = arr[right], arr[left]
+        return self.recReverse(arr,left+1,right-1)
+
+arr = [1,2,3,4,5]
 
 fact = RecursionProblems("Cow")
-print(fact.factorial(3))
-print(fact.fib(6))
+# print(fact.factorial(3))
+# print(fact.fib(6))
+# print(fact.ascPrintNum(1,5))
+# print("Sum ", fact.sumFirstNum(3))
+fact.recReverse(arr, 0 ,len(arr)-1)
+print("arr",arr)
--- Codes/LearnTheBasic/Lec2/BuildLogicalThinking.py
+++ Codes/LearnTheBasic/Lec2/BuildLogicalThinking.py
@@ -1,12 +1,39 @@
-# Patterns
-'''
-***********
-***********
-***********
-***********
-***********'''
+class Patterns:
 
+    def __init__(self, name):
+        self.name = name
 
-for i in range(4):
-    for j in i:
-        print("*")
+    def square(self,n):
+        for i in range(n):
+            for j in range(n):
+                print("*", end = "")
+            print()
+
+    def triangle(self,n):
+        for i in range(n):
+            for j in range(i+1):
+                print("*", end = "")
+            print()
+
+    def inverseTriangle(self,n):
+        for i in range(n):
+            for j in range(i+1):
+                print(" ", end = "")
+            for j in range(n-i):
+                print("*", end = "")
+            for j in range(n-i):
+                print("*", end = "")
+            print()
+        
+        for i in range(n):
+            for j in range(n-i):
+                print(" ", end = "")
+            for j in range(i+1):
+                print("*", end = "")
+            for j in range(i+1):
+                print("*", end = "")
+            print()
+name = Patterns('name')
+# name.square(5)
+# name.triangle(5)
+name.inverseTriangle(5)
